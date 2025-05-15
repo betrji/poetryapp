6523694792cf1d834adacb79e5045a85d667cb70
betrji <adhitanya@gmail.com>
2025-05-14 22:41:44 -0400

    Update JapaneseTheme.css with proper CSS styles

--- eslint.config.js
+++ eslint.config.js
@@ -2,37 +2,28 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tsPlugin from '@typescript-eslint/eslint-plugin'
-import tsParser from '@typescript-eslint/parser'
 
 export default [
   { ignores: ['dist'] },
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.{js,jsx}'],
     languageOptions: {
-      parser: tsParser,
+      ecmaVersion: 2020,
+      globals: globals.browser,
       parserOptions: {
         ecmaVersion: 'latest',
-        sourceType: 'module',
         ecmaFeatures: { jsx: true },
-        project: './tsconfig.json',
-      },
-      globals: {
-        ...globals.browser,
-        React: 'readonly',
+        sourceType: 'module',
       },
     },
     plugins: {
-      '@typescript-eslint': tsPlugin,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
       ...js.configs.recommended.rules,
-      ...tsPlugin.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
